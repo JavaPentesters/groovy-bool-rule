12283c21fce00ff47d841edd46eb0d6bbb66b9c2
luyb <luyb@servyou.com.cn>
2020-08-14 15:49:14 +0800

    update README.md

---
+++ src/main/java/com/geekmake/groovy/rules/model/Event.java
@@ -0,0 +1,8 @@
+package com.geekmake.groovy.rules.model;
+
+/**
+ * @author pez1420@gmail.com
+ * @version $Id: Event.java v 0.1 2020/8/14 10:57 上午 pez1420 Exp $$
+ */
+public class Event {
+}
---
+++ src/main/java/com/geekmake/groovy/rules/model/Index.java
@@ -0,0 +1,8 @@
+package com.geekmake.groovy.rules.model;
+
+/**
+ * @author pez1420@gmail.com
+ * @version $Id: Index.java v 0.1 2020/8/14 10:58 上午 pez1420 Exp $$
+ */
+public class Index {
+}
